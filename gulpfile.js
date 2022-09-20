ade0aaac129e6c21b65c93c5dad119c6d1d53b86
MAllena <MAllena@rosariobus.com.ar>
2022-09-20 12:27:04 -0300

    trabajando_las_imagenes_gulp_webp

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,17 @@
 
 exports.primerTarea = tarea; */
 
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+//css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//imagenes
+const webp = require('gulp-webp');
+
+
+//? FUNCION QUE IDENTIFICA, COMPILA Y ALMACENA TODOS LOS ARCHIVOS SCSS A CSS
 
 function css(done)  {
     // Identificar el archivo sass
@@ -21,11 +29,30 @@ function css(done)  {
     done();//callback que avisa a gulp que llegamos al final
 }
 
+//? OBSERVA TODOS LOS ARCHIVOS SCSS Y LLAMA LA FUNCION (CSS)
 function dev(done) {
     watch('src/scss/**/*.scss', css)
 
     done();
 }
 
+
+//? FUNCION PARA AUTOMATIZAR LA CONVERSION DE IMAGENES AL FORMATO WEBP
+
+function versionWebp(done) {
+    //opciones de calidad para las imagenes (va de 0 a 100)
+    const opciones = {
+        quality:50
+    }
+
+    //scr es la ruta donde estan todas las imagenes
+    src('src/img/**/*.{jpg,png}')
+    .pipe( webp(opciones)) //paso opciones como parametro para que toma la quality
+    .pipe( dest('/build/img')) //destino donde se guardan las imagenes
+
+    done() //avisa que la funcion terminó
+}
+
 exports.css = css;
-exports.dev = dev;
+exports.dev = parallel(versionWebp, dev); //paralell ejecuta las tareas en paralelo 
+exports.versionWebp = versionWebp;
